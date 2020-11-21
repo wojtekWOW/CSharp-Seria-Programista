068990a56d7b0857a3a4a794d2363f928b34e25d
wojciechstachyra.wow@gmail.com <wojciechstachyra.wow@gmail.com>
2020-11-21 13:09:42 +0100

    Hello program

---
+++ Hello/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpsiz swoje imię: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Witaj {name}!");
+            Console.Write("Wpisz ile masz lat:");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Jesteś pełnoletni możesz wypić browara!");
+            }
+            else
+            {
+                Console.WriteLine("Możemy Ci zaproponować mleko");
+            }
+
+        }
+    }
+}
