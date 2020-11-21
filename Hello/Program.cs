e139628971d2ab9b3f5334510de3043eb258d6fa
wojciechstachyra.wow@gmail.com <wojciechstachyra.wow@gmail.com>
2020-11-21 13:24:21 +0100

    Indefinite for loop implementation, Console text color changes, clearing and reseting console

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,19 +6,34 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.Write("Wpsiz swoje imię: ");
-            string name = Console.ReadLine();
+            for (; ;)
+            {
+                Console.Write("Wpsiz swoje imię: ");
+                string name = Console.ReadLine();
 
-            Console.WriteLine($"Witaj {name}!");
-            Console.Write("Wpisz ile masz lat:");
-            int age = int.Parse(Console.ReadLine());
-            if (age >= 18)
-            {
-                Console.WriteLine("Jesteś pełnoletni możesz wypić browara!");
-            }
-            else
-            {
-                Console.WriteLine("Możemy Ci zaproponować mleko");
+                Console.WriteLine($"Witaj {name}!");
+                Console.Write("Wpisz ile masz lat:");
+                int age = 0;
+                bool adult = int.TryParse(Console.ReadLine(), out age);
+                if (age >= 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Jesteś pełnoletni możesz wypić browara!");
+                }
+                else if (adult == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wprowadziłeś niepoprawne dane");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Możemy Ci zaproponować mleko");
+                }
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+            
             }
 
         }
