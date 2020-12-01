803dd5f76313c6c652df2966faf934436067bb91
wojciechstachyra.wow@gmail.com <wojciechstachyra.wow@gmail.com>
2020-12-01 15:07:20 +0100

    typy wartosciowe i referencyjne

---
+++ Typy/Diary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+        //Parametry=pola
+    
+        List<float> ratings;
+        public string nazwaObiektu;
+        string imie;
+
+        //Metody
+       
+        // Computes statitistic (average, max and min grade) for given student
+        internal DiaryStatistic ComputeStatistic()
+        {
+            DiaryStatistic stats = new DiaryStatistic();
+            float sum = 0f;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            stats.AverageGrade = sum / ratings.Count();
+            stats.MaxGrade= ratings.Max();
+            stats.MinGrade = ratings.Min();
+            return stats;
+        }    
+              
+        /// <summary>
+        /// Method adding new rating to diary
+        /// </summary>
+        /// <param name="rating">new rating</param>
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }      
+       
+        /// <summary>
+        /// Method to add student's name
+        /// </summary>
+        /// <returns>imie</returns>
+        public string AddName()
+        {
+            Console.WriteLine("Podaj imie studenta:");
+                imie = Console.ReadLine();
+            return imie;
+        }
+
+        /// <summary>
+        /// Method displaying instructions, uses other methods to display answers
+        /// </summary>
+        /// <param name="student"></param>
+        public static void AskForRatings(Diary student)
+        {            
+            Console.WriteLine($"Podaj oceny ucznia z zakresu 1-10 uzyskane przez  {student.imie}");
+            for (; ; )
+            {        
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+        
+                if (rating == 11)
+                {
+                    break;
+                }
+                if (result == true)
+                {
+                    if (rating >= 1 && rating <= 10)
+                        student.AddRating(rating);
+                    else Console.WriteLine("Niepoprawna liczba1 Podaj liczbę z zakresu od 1 do 10");
+                }
+            }       
+        }
+
+    }
+}
---
+++ Typy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        private static void GiveName(ref Diary diary)
+        {
+            diary = new Diary();
+            diary.nazwaObiektu="Nowy dzienniczek Wojtka";
+        }
+
+        private static int IncrementNumber(int number)
+        {
+            number = 32;
+            return number;
+        }
+        static void Main(string[] args)
+        {            
+            Diary d1 = new Diary();
+            Diary d2 = d1;
+
+            GiveName(ref d2);
+                        
+            Console.WriteLine(d2.nazwaObiektu);
+            
+            int x1 = 4;
+            x1 = IncrementNumber(x1);
+            Console.WriteLine(x1);
+
+            DateTime d;
+            string name = "Marcin";
+            string name1 = "marcin";
+            bool areEqual = name.Equals(name1, StringComparison.CurrentCultureIgnoreCase);
+            Console.WriteLine(areEqual);
+        }       
+    }
+}
