809a3235cffa7a8d04f0c6e3ba25c9631d94b795
wojciechstachyra.wow@gmail.com <wojciechstachyra.wow@gmail.com>
2021-01-08 18:09:18 +0100

    flow control with if statements

---
+++ FlowControl/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl
+{
+    class Program
+    {
+        private static int age;
+        private static string name;
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Podaj swój wiek");
+            age = int.Parse(Console.ReadLine());
+
+            string test = age > 18 ? "Pełnoletni" : "Dziecko";
+
+            Console.WriteLine(test);
+
+            Console.WriteLine("Podaj swoje imie");
+            Console.ReadLine();
+
+            if (age<=10)
+            {
+                if (name == "Marcin")
+                {
+
+                }
+                Method1();
+            }
+            else if (age<20)
+            {
+                Method2();
+            }
+            else if (age <= 30)
+            {
+                Method3();
+            }
+            else
+            {
+                Method4();
+            }
+            Console.ReadKey();
+        }
+
+        private static void Method4()
+        {
+            Console.WriteLine($"Twój wiek jest wiekszy 30 lat i wynosi {age} lat");
+        }
+
+        private static void Method3()
+        {
+            Console.WriteLine($"Twój wiek jest wiekszy 20 lat i mniejszy lub równy 30 lat i wynosi {age} lat");
+        }
+
+        private static void Method2()
+        {
+            Console.WriteLine($"Twój wiek jest wiekszy 10 lat i mniejszy niż 20 lat i wynosi {age} lat");
+        }
+
+        private static void Method1()
+        {
+            Console.WriteLine("Masz 10 lub mniej lat"); ;
+        }
+    }
+}
